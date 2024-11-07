4ba1af25e1f978b2aecaff577e74addb48541b59
Edward Leon <edleon761@gmail.com>
2024-11-06 22:27:23 -0800

    Chekpoint
    - Renamed scene manager script
    - Added a credits scene

--- Assets/Scripts/Managers/SceneManagerScript.cs
+++ Assets/Scripts/Managers/SceneManagerScript.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneManagerScript : MonoBehaviour
 {
 
-    public static SceneManager Instance;
+    public static SceneManagerScript Instance;
 
     public void Awake()
     {
@@ -22,4 +22,18 @@ public class SceneManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    // Loads a scene depedent on the value passed in
+    /*
+     * 0 == main menu
+     * 1 == settings
+     * 2 == character customization
+     * 3 == main game
+     * 4 == credits
+     */
+    public void LoadScene(int val)
+    {
+        SceneManager.LoadScene(val);
+    }
+
 }
