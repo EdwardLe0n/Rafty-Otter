9a6c5dca25e5f0a69eff7c39ab095f6d0f618954
afonc <37196107+Af0n@users.noreply.github.com>
2024-11-07 21:45:55 -0800

    Dynamic menu size for character display

--- Assets/Scripts/UI/CharacterGridSize.cs
+++ Assets/Scripts/UI/CharacterGridSize.cs
@@ -1,5 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CharacterGridSize : MonoBehaviour {
-    
+public class CharacterGridSize : MonoBehaviour 
+{
+    private void Update()
+    {
+        EvaluateCellSize();
+        EvaluateHeight();    
+    }
+
+    private void EvaluateHeight()
+    {
+        RectTransform rform = GetComponent<RectTransform>();
+        GridLayoutGroup grid = GetComponent<GridLayoutGroup>();
+
+        int childCount = transform.childCount;
+        int rowCount = 1 + childCount / 3;
+
+        float height = (grid.cellSize.y * rowCount) + grid.spacing.y * (rowCount - 1) + 4 * grid.padding.top;
+
+        rform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+    }
+
+    private void EvaluateCellSize()
+    {
+        RectTransform rform = GetComponent<RectTransform>();
+        GridLayoutGroup grid = GetComponent<GridLayoutGroup>();
+
+        float cellWidth = transform.parent.GetComponent<RectTransform>().rect.width / 3.5f;
+        float cellHeight = transform.parent.GetComponent<RectTransform>().rect.height / 4.5f;
+        float spacing = 20 * (cellWidth/cellHeight);
+
+        grid.cellSize = new Vector2(cellWidth, cellHeight);
+        grid.spacing = new Vector2(spacing, spacing);
+        grid.padding.top = (int)(spacing / 2f);
+        grid.padding.bottom = grid.padding.top;
+    }
 }
